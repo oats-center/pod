e74d89723d06a4185f119175a934da2bc6b3be0f
Andrew Balmos <abalmos@purdue.edu>
2022-04-27 14:30:19 -0400

    Rain Gauge GPS codec fix

--- codecs/tektelic_surface_codec.js
+++ codecs/tektelic_surface_codec.js
@@ -4,98 +4,157 @@ function Decode(port, bytes, variables) {
     data: bytes,
     warnings: [],
   };
-  // decode: decode(sensor, bytes, port, true)
-  switch (port) {
-    // fPort 10
-    case 10:
-      // Loop over all variables in the message, decoding one at a time.
-      var offset = 0;
-      while (offset < bytes.length) {
-        // Get key / header for this data point
-        var key = getUint16(bytes, offset);
+
+  /////////////////
+  // SENSOR NODE //
+  /////////////////
+
+  // Loop over all variables in the message, decoding one at a time.
+  var offset = 0;
+  while (offset < bytes.length) {
+    var key;
+
+    // Get key / header for this data point
+    if (offset === 0) {
+      key = port;
+    } else {
+      key = getUint8(bytes, offset);
+      offset += 1;
+    }
+
+    // Process the data based on the data type key
+    switch (key) {
+      // Verison
+      case 1:
+        output.product_id = getUint8(bytes, offset);
+        offset += 1;
+
+        output.hardware_rev = getUint8(bytes, offset);
+        offset += 1;
+
+        output.firmware_major = getUint8(bytes, offset);
+        offset += 1;
+
+        output.firmware_minor = getUint8(bytes, offset);
+        offset += 1;
+        break;
+
+      // GPS
+      case 10:
+        output.lat = getInt24(bytes, offset) * 0.0000256;
+        offset += 3;
+
+        output.lng = getInt24(bytes, offset) * 0.0000256;
+        offset += 3;
+        break;
+
+      // Battery voltage
+      case 20:
+        output.battery_voltage = getUint16(bytes, offset) / 1000;
+        offset += 2;
+        break;
+
+      // Analog in 1
+      case 21:
+        output.analog_in_1 = getUint16(bytes, offset) / 1000;
+        offset += 2;
+        break;
+
+      // Analog in 2
+      case 22:
+        output.analog_in_2 = getUint16(bytes, offset) / 1000;
+        offset += 2;
+        break;
+
+      // Input 1 pulse count
+      case 31:
+        output.input1_pulse_count = getUint16(bytes, offset);
+        offset += 2;
+        break;
+
+      // Input 2 pulse count
+      case 32:
+        output.input2_pulse_count = getUint16(bytes, offset);
+        offset += 2;
+        break;
+
+      case 39:
+        output.current_io_state = getUint8(bytes, offset);
+        offset += 1;
+
+        output.alert_input = getUint8(bytes, offset);
+        offset += 1;
+
+        output.input1_pulse_count = getUint16(bytes, offset);
         offset += 2;
 
-        // Process the data based on the data type key
-        switch (key) {
-          // Battery
-          case 0x00ba:
-            var value = getUint8(bytes, offset);
-            offset += 1;
+        output.input2_pulse_count = getUint16(bytes, offset);
+        offset += 2;
+        break;
 
-            output.battery_voltage = (value & 0x7f) * 0.01 + 2.5;
-            output.end_of_service = (value & 0x80) !== 0;
-            break;
+      // Internal temperature
+      case 40:
+        output.internal_temp = getUint16(bytes, offset) / 100;
+        offset += 2;
+        break;
 
-          // Soil GWC
-          case 0x0104:
-            var value = getUint16(bytes, offset);
-            offset += 2;
+      default:
+        output.warnings.push("unknown port or register address: " + key);
+        break;
+    }
+  }
 
-            output.soil_moisture_hz = value * 1000;
-            output.soil_moisture_gwc = convertSoilGWC(output.soil_moisture_hz);
-            break;
+  ////////////////////////
+  // SENSOR CONVERSIONS //
+  ////////////////////////
 
-          // Soil temperature
-          case 0x0202:
-            var value = getUint16(bytes, offset);
-            offset += 2;
+  // VH400
+  if (output.analog_in_1) {
+    var vwc = +vegetronixVWC(output.analog_in_1).toFixed(2);
 
-            output.soil_temp0_raw = value;
+    if (vwc < 0) {
+      output.warnings.push(
+        "Invalid VH400 sensor range (" +
+          output.analog_in_1 +
+          ") on input 1. Is it connected?"
+      );
+    } else {
+      output.volumetric_water_content_1 = vwc;
+    }
+  }
 
-            value = -31.96 * Math.log(value) + 213.25;
-            output.soil_temp0_c = +value.toFixed(2);
-            break;
+  // NTC 10k 3977K Temperature Sensor (e.g., TT02-10KC3-T105-1500)
+  // Wiring guide:
+  // Vcc ---- Rthem ------- Rbal ------ GND
+  //                   |
+  //                   |
+  //               analog_in_2
+  // Assume:
+  //  - 10k balance resistor
+  //  - 3977 beta
+  //  - 10k thermistor @ 25 C
+  if (output.analog_in_2) {
+    var Vs = 3.3; // Vcc voltage
+    var Rbal = 10000; // Balance resistor value
 
-          // Light Intensity
-          case 0x0965:
-            var value = getUint16(bytes, offset);
-            offset += 2;
+    /////////////////////
+    // Sensor specific //
+    /////////////////////
+    var beta = 3977; // Device constant
+    var T0 = 298.15; // Kelvin (25 C)
+    var R0 = 10000; // Thermistor resistance @ T0
 
-            output.light_intensity_lux = value;
-            break;
+    // Steinhart-Hart Beta equation
+    var temp_k =
+      (T0 * beta) /
+      (beta + T0 * Math.log((Rbal * (Vs / output.analog_in_2 - 1)) / R0));
 
-          // Ambient Temp
-          case 0x0b67:
-            var value = getInt16(bytes, offset);
-            offset += 2;
+    output.temperature_c = +(temp_k - 273.15).toFixed(2);
+  }
 
-            output.ambient_temp_raw = value;
-            output.ambient_temp_c = +(value * 0.1).toFixed(2);
-            break;
-
-          // Relative Humidity
-          case 0x0b68:
-            var value = getUint8(bytes, offset);
-            offset += 1;
-
-            output.relative_humidity_raw = value;
-            output.relative_humidity = +(value * 0.5).toFixed(2);
-            break;
-
-          // MCU Temperature
-          case 0x0c67:
-            var value = getInt16(bytes, offset);
-            offset += 2;
-
-            output.mcu_temp_raw = value;
-            output.mcu_temp_c = +(value * 0.1).toFixed(2);
-            break;
-
-          default:
-            output.warnings.push("Unknown data key: " + key.toString(16));
-        }
-      }
-
-      break;
-
-    // fPort 100
-    case 100:
-      // FIXME: Should deal with config uplinks someday ...
-      output.warnings.push("Config uplinks not implemented");
-      break;
-
-    default:
-      output.warnings.push("Unknown port: " + port);
+  // Davis AeroCone Rain Gauge (6466)
+  if (output.input1_pulse_count) {
+    output.total_rain_in = +(output.input1_pulse_count * 0.01).toFixed(2);
   }
 
   return output;
@@ -104,22 +163,15 @@ function Decode(port, bytes, variables) {
 //////////////////////////
 // CONVERSION FUNCTIONS //
 //////////////////////////
-function convertSoilGWC(value) {
-  value = value / 1000; /// threshold on kHz
-
-  if (value < 1346) return 1.2;
-  if (value < 1351 && value >= 1346) return 1.1;
-  if (value < 1356 && value >= 1351) return 1.0;
-  if (value < 1361 && value >= 1356) return 0.9;
-  if (value < 1366 && value >= 1361) return 0.8;
-  if (value < 1371 && value >= 1366) return 0.7;
-  if (value < 1376 && value >= 1371) return 0.6;
-  if (value < 1381 && value >= 1376) return 0.5;
-  if (value < 1386 && value >= 1381) return 0.4;
-  if (value < 1391 && value >= 1386) return 0.3;
-  if (value < 1396 && value >= 1391) return 0.2;
-  if (value < 1399 && value >= 1396) return 0.1;
-  if (value < 1402 && value >= 1399) return 0;
+//
+// Source: https://www.vegetronix.com/Products/VH400/VH400-Piecewise-Curve
+function vegetronixVWC(value) {
+  if (value < 0) return -1;
+  if (value <= 1.1) return 10 * value - 1;
+  if (value <= 1.3) return 25 * value - 17.5;
+  if (value <= 1.82) return 48.08 * value - 47.5;
+  if (value <= 2.2) return 26.32 * value - 7.89;
+  if (value <= 3.0) return 62.5 * value - 87.5;
 
   return -1;
 }
@@ -132,10 +184,28 @@ function getUint8(bytes, offset) {
 }
 
 function getUint16(bytes, offset) {
-  return bytes[offset] * 256 + bytes[offset + 1];
+  return bytes[offset + 1] * 256 + bytes[offset];
 }
 
-function getInt16(bytes, offset) {
-  // Properly sign extend if needed
-  return ((bytes[offset] << 24) >> 16) | bytes[offset + 1];
+function getInt24(bytes, offset) {
+  var v = bytes[offset] * 65536 + bytes[offset + 1] * 256 + bytes[offset + 2];
+
+  if ( (v & 0x800000) > 0 ) {
+    v -= 0x1000000;
+  }
+  
+  return v;
+}
+
+function getUint24(bytes, offset) {
+  return bytes[offset] * 65536 + bytes[offset + 1] * 256 + bytes[offset + 2];
+}
+
+function getUint32(bytes, offset) {
+  return (
+    bytes[offset + 3] * 16777216 +
+    bytes[offset + 2] * 65536 +
+    bytes[offset + 1] * 256 +
+    bytes[offset + 3]
+  );
 }
